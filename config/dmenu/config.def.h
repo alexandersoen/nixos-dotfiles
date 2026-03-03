917d76d7b146d87196ae7ce3c89852eede8f596d
alexandersoen <alexandersoen@gmail.com>
2026-03-03 23:52:54 +0100

    Some dmenu patches

--- config/dmenu/config.def.h
+++ config/dmenu/config.def.h
@@ -2,6 +2,10 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=10"
@@ -11,6 +15,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -21,3 +27,6 @@ static unsigned int lines      = 10;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
